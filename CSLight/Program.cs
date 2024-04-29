e82b5cee373390258b06df317b77b5c18b69c932
Nick <qdqd@acvvrv.com>
2024-04-29 14:44:04 +0300

    Add project files.

---
+++ CSLight/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Types
+            // Integer
+            byte b; // 0 to 255
+            sbyte sb; // -128 to 127
+            short s; // -32768 to 32767
+            ushort us; // 0 to 65535
+            int i; // -2147483648 to 2147483647
+            uint ui; // 0 to 4294967295
+            long l; // -9223372036854775808 to 92233
+            ulong ul; // 0 to 18446744073709551615
+            // Floating point
+            float f; // 32 bit IEEE 754 floating point = 1.0f
+            double d; // 64 bit IEEE 754 floating point = 1.0d
+            decimal de; // 128 bit IEEE 754 floating point = 1.0m
+            // Character
+            char c; // 16 bit Unicode character 'a', 'A', '1', '!', 'ö', 'Ж'
+            // String
+            string str; // UTF-16 encoded string "Hello World"
+            // Boolean
+            bool bl; // true or false
+            // Null
+            object o; // null
+
+            int age = 25;
+
+            age = 26;
+
+        }
+    }
+}
