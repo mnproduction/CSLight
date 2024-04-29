d529b1e40c4dc89f441fafa7a8cfa464171569dc
Nick <qdqd@acvvrv.com>
2024-04-30 02:43:47 +0300

    Switch - case

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,38 +10,35 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //If Else
-
-            int age;
-
-            Console.Write("Enter your age: ");
-
-            age = Convert.ToInt32(Console.ReadLine());
-
-            if (age >= 18)
-            {
-                Console.WriteLine("You are an adult");
-                Console.WriteLine("You can vote");
-            }
-            else
-            {
-                Console.WriteLine("You are not an adult");
-                Console.WriteLine($"Come again in {18 - age} years");
-            }
+            //Switch
 
             string dayOfWeak;
 
             Console.Write("Enter the day of the week: ");
             dayOfWeak = Console.ReadLine();
 
-            if (dayOfWeak == "Monday".ToLower())
+
+            switch (dayOfWeak.ToLower())
             {
-                Console.WriteLine("It's Monday");
+                case "monday":
+                    Console.WriteLine("It's Monday"); break;
+                case "tuesday":
+                    Console.WriteLine("It's Tuesday"); break;
+                case "wednesday":
+                    Console.WriteLine("It's Wednesday"); break;
+                case "thursday":
+                    Console.WriteLine("It's Thursday"); break;
+                case "friday":
+                    Console.WriteLine("It's Friday"); break;
+                case "saturday":
+                case "sunday":
+                    Console.WriteLine("It's Weekend!!!"); break;
+                default:
+                    Console.WriteLine("Invalid day of the week"); break;
             }
-            else if (dayOfWeak == "Tuesday".ToLower())
-            {
-                Console.WriteLine("It's Tuesday");
-            }
+
+
+
 
 
 
