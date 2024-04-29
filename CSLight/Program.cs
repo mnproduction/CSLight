56c67288a24d90252ed64c7c046523f8b4476ba7
Nick <qdqd@acvvrv.com>
2024-04-30 00:55:44 +0300

    Console read and write

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,39 +10,10 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            // Strings
+            // Console
 
-            // Concat
-
-            Console.WriteLine("Hello" + " " + "World");
-
-            // Interpolation
-
-            Console.WriteLine($"Hello {1 + 1} World");
-
-            // Conversions
-
-            int age;
-            
-            string ageAsString = "25";
-
-            age = Convert.ToInt32(ageAsString);
-            Console.WriteLine(Convert.ToChar(age));
-
-            
-            Console.WriteLine(Convert.ToDouble(ageAsString));
-            
-
-            Console.WriteLine(Convert.ToByte(age));
-
-
-            Console.WriteLine(Convert.ToDecimal(age));
-
-
-            Console.WriteLine(Convert.ToBoolean(age));
-
-
-            Console.WriteLine();
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
 
 
 
