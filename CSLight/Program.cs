6460c18b96e6cfc5a92425625c1d4435f5dd18fb
Nick <qdqd@acvvrv.com>
2024-04-30 02:33:55 +0300

    If Else

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,61 +10,45 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            // Increment and Decrement
+            //If Else
 
-            int a = 0;
+            int age;
 
-            // postfix
-            Console.WriteLine(a++);
-            // prefix
-            Console.WriteLine(a);
-            Console.WriteLine(++a);
+            Console.Write("Enter your age: ");
 
-            double health;
-            int damage;
-            int armor;
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are an adult");
+                Console.WriteLine("You can vote");
+            }
+            else
+            {
+                Console.WriteLine("You are not an adult");
+                Console.WriteLine($"Come again in {18 - age} years");
+            }
+
+            string dayOfWeak;
+
+            Console.Write("Enter the day of the week: ");
+            dayOfWeak = Console.ReadLine();
+
+            if (dayOfWeak == "Monday".ToLower())
+            {
+                Console.WriteLine("It's Monday");
+            }
+            else if (dayOfWeak == "Tuesday".ToLower())
+            {
+                Console.WriteLine("It's Tuesday");
+            }
 
 
-            // reading data from the user
-            Console.Write("Enter your health: ");
-            health = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter your armor: ");
-            armor = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter damage: ");
-            damage = Convert.ToInt32(Console.ReadLine());
-
-            // calculating the damage received
-            double recievedDamage = damage * (1 - armor * 0.01);
 
 
-            // updating the player's health
-            health -= recievedDamage;
 
-            Console.WriteLine($"Your recieved {damage} damage and your health is {health}");
 
-            int money;
-            int food;
-            int foodUnitPrice = 10;
-            
 
-            Console.WriteLine($"Grettings wonderer! Welcome to our backery store! Today we have food for {foodUnitPrice} gold coins!");
-            Console.Write("How much gold coins do you have?: ");
-
-            money = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("How much food units do you want?: ");
-
-            food = Convert.ToInt32(Console.ReadLine());
-
-            bool isEnoughMoney = money >= foodUnitPrice * food;
-
-            food *= Convert.ToInt32(isEnoughMoney);
-
-            money -= food * foodUnitPrice;
-
-            Console.WriteLine($"You bought {food} food units for {food * foodUnitPrice} gold coins! You have now {money} gold coins left!");
 
 
             Console.ReadKey();
