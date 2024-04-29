ac3a7ef47dec7bafc87e2cc68aa350287bdc97f5
Nick <qdqd@acvvrv.com>
2024-04-30 02:04:42 +0300

    Increment and Decrement with examples

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -13,12 +13,58 @@ namespace CSLight
             // Increment and Decrement
 
             int a = 0;
-            
-            Console.WriteLine(a++); // postfix
+
+            // postfix
+            Console.WriteLine(a++);
+            // prefix
             Console.WriteLine(a);
-            Console.WriteLine(++a); // prefix
+            Console.WriteLine(++a);
+
+            double health;
+            int damage;
+            int armor;
 
 
+            // reading data from the user
+            Console.Write("Enter your health: ");
+            health = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your armor: ");
+            armor = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter damage: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+
+            // calculating the damage received
+            double recievedDamage = damage * (1 - armor * 0.01);
+
+
+            // updating the player's health
+            health -= recievedDamage;
+
+            Console.WriteLine($"Your recieved {damage} damage and your health is {health}");
+
+            int money;
+            int food;
+            int foodUnitPrice = 10;
+            
+
+            Console.WriteLine($"Grettings wonderer! Welcome to our backery store! Today we have food for {foodUnitPrice} gold coins!");
+            Console.Write("How much gold coins do you have?: ");
+
+            money = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How much food units do you want?: ");
+
+            food = Convert.ToInt32(Console.ReadLine());
+
+            bool isEnoughMoney = money >= foodUnitPrice * food;
+
+            food *= Convert.ToInt32(isEnoughMoney);
+
+            money -= food * foodUnitPrice;
+
+            Console.WriteLine($"You bought {food} food units for {food * foodUnitPrice} gold coins! You have now {money} gold coins left!");
 
 
             Console.ReadKey();
