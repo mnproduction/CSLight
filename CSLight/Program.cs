75461c9a5b7d84cf71e75822597503d57414b58d
Nick <qdqd@acvvrv.com>
2024-04-30 06:05:08 +0300

    Functions

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,41 +10,28 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            //Guess a number from 1 to 100
+            // Functions
 
-            Random random = new Random();
-            int value;
-            int triesCount = 5;
-            int userInput;
-            
-            value = random.Next(1, 101);
-            Console.Write("Guess the number from 1 to 100: ");
 
-            while (triesCount-- > 0)
-            {
-                userInput = Convert.ToInt32(Console.ReadLine());
-                if (userInput == value && triesCount > 0)
-                {
-                    Console.WriteLine("You guessed the right number!");
-                    break;
-                }
-                else if (userInput > value)
-                {
-                    Console.WriteLine("Your guess is too high!");
-                }
-                else if (userInput < value)
-                {
-                    Console.WriteLine("Your guess is too low!");
-                }
-                else { Console.WriteLine("Invalid input!"); }
-
-                if (triesCount == 0) { Console.WriteLine($"You've run out of tries! The number was {value}"); break; }
-            }
+            Console.WriteLine($"Hi!");
+            Console.WriteLine("How are you?");
+            Console.WriteLine("I'm fine, thank you!");
+            WriteError(message: "Now I'm going to write an error!");
+            Console.WriteLine("Goodbye!");
+            WriteError();
 
 
             Console.ReadKey();
 
 
+        }
+
+        static void WriteError(string message = "Error!")
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = defaultColor;
 
 
         }
