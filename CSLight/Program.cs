e0679eb0efa7448fb4b533f56ff44dfe7bfea985
Nick <qdqd@acvvrv.com>
2024-05-01 21:35:18 +0300

    Has a... connection

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,96 +10,59 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            bool isOpen = true;
-            
-            Table[] tables = {new Table(1, 4), new Table(2, 8), new Table(3, 10)};
+            Performer worker1 = new Performer("John");
+            Performer worker2 = new Performer("Jane");
 
-            while (isOpen)
-            {
-                foreach (Table table in tables)
-                {
-                    table.ShowPlaces();
-                }
-                Console.WriteLine("\n1. Reserve places\n2. Cancel reservation\n3. Exit");
-                int choice = int.TryParse(Console.ReadLine(), out int choiceParse) ? choiceParse : 0;
+            Task[] tasks = { new Task(worker1, "Dig a hole"), new Task(worker2, "Throw ground away") };
 
+            Board schedule = new Board(tasks); 
 
-                switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine("Which table do you want to take a place at?");
-                        int tableReservation = Convert.ToInt32(Console.ReadLine()) - 1;
-                        Console.WriteLine("How many places do you want to take?");
-                        int numPlacesReserve = Convert.ToInt32(Console.ReadLine());
-                        tables[tableReservation].TakePlace(tableReservation, numPlacesReserve);
-                        continue;
-                    case 2:
-                        Console.WriteLine("Which table do you want to cancel a reservation at?");
-                        int tableCancel = Convert.ToInt32(Console.ReadLine()) - 1;
-                        Console.WriteLine("How many places do you want to cancel?");
-                        int numPlacesCancel = Convert.ToInt32(Console.ReadLine());
-                        tables[tableCancel].TakePlace(tableCancel, numPlacesCancel);
-                        continue;
-                    case 3:
-                        isOpen = false;
-                        continue;
-                    default:
-                        continue;
-
-                Console.ReadKey();
-                Console.Clear();
-                }
-
-
-            }
-
-
-
+            schedule.ShowTasks();
 
 
             Console.ReadKey();
-
-
         }
-
-        
-
-
     }
 
-    class Table
+    class Performer
     {
-        public int Id;
-        public int MaxPlaces;
-        public int FreePlaces;
+        public string Name;
+        
+        public Performer(string name) { Name = name; }
+    }
 
-        public Table(int id, int maxPlaces)
-        {
-            Id = id;
-            MaxPlaces = maxPlaces;
-            FreePlaces = maxPlaces;
+    class Board
+    {
+        public Task[] Tasks;
+
+        public Board(Task[] tasks) 
+        { 
+            Tasks = tasks; 
         }
 
-        public void ShowPlaces()
+        public void ShowTasks()
         {
-            Console.WriteLine($"Table {Id} has {FreePlaces} of {MaxPlaces} places");
-        }
-
-        public void TakePlace(int tableId, int numPlaces)
-        {
-            if (numPlaces <= this.FreePlaces)
+            foreach (var task in Tasks)
             {
-                this.FreePlaces -= numPlaces;
-                Console.WriteLine($"You have taken {numPlaces} place/s at table #{tableId}\nIt now has {this.FreePlaces} of {this.MaxPlaces} places");
+                task.ShowInfo();
             }
-            else if (numPlaces > this.FreePlaces) 
-            { 
-                Console.WriteLine($"You can't take {numPlaces} place/s at table #{tableId}, as there are only {this.FreePlaces} places left"); 
-            }
-            else 
-            { 
-                Console.WriteLine($"Table {tableId} is full"); 
-            }
+        }
+    }
+
+    class Task
+    {
+        public Performer Worker;
+        public string Description;
+
+        public Task(Performer worker, string description)
+        {
+            Worker = worker;
+            Description = description;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Description: {Description}\nResponsable:{Worker.Name}");
         }
     }
 
